2901d6083d0c86d5d4150fe056f073a711938463
meredithcat <meredithcat@gmail.com>
2021-01-28 10:47:16 -0800

    Update routes.py

--- books_app/routes.py
+++ books_app/routes.py
@@ -1,6 +1,5 @@
 """Import packages and modules."""
 import os
-import requests
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
 from pprint import PrettyPrinter
